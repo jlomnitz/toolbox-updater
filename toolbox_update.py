94af873654470b9fc951b80913fc5b1cfbb22050
Jason Lomnitz <jlomn@ucdavis.edu>
2014-10-07 16:27:35 -0700

    Default behavior changed to build latest relese version

--- toolbox_update.py
+++ toolbox_update.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/python
 
 from subprocess import call, Popen, PIPE
 from distutils.version import StrictVersion
@@ -10,14 +10,14 @@ __version__ = '0.2.0'
 
 STABLE_VERSION='develop'
 
-DEFAULT_TOOLBOX_VERSION = 'v0.2.1'
-DEFAULT_INTERFACE_VERSION = 'v0.2.0'
+DEFAULT_TOOLBOX_VERSION = '$RELEASE'
+DEFAULT_INTERFACE_VERSION = '$RELEASE'
 
 DESCRIPTION_STRING = \
 ''' Script to update the Design Space Toolbox V2 and its
 python interface. The script switches to the appropriate branch and 
 pulls from origin and runs make/xcodebuild to install depending on 
-linux/mac system.'''
+linux/mac system. The command builds the latest release as default.'''
 
 def parse_arguments(): 
     parser = argparse.ArgumentParser(prog='Design space toolbox and interface updater',
@@ -26,8 +26,18 @@ def parse_arguments():
                         const='stable',
                         help='build latest stable version')
     parser.add_argument('--release', dest='stable_or_release', action='store_const',
-                        const='release',
+                        const='release', default='release',
                         help='build latest release version')
+    parser.add_argument('--toolbox_version', dest='toolbox_version',
+                        default=DEFAULT_TOOLBOX_VERSION, 
+                        type=str,
+                        help='build a specific version or branch of the c library')
+    parser.add_argument('--interface_version', dest='interface_version',
+                        default=DEFAULT_INTERFACE_VERSION, 
+                        type=str,
+                        help='build a specific version or branch of the python interface')
+    parser.add_argument('--linux', dest='is_linux', action='store_true',
+                        help='indicates if it should build using linux commands')
     parser.add_argument('--toolbox_dict', dest='toolbox_dict', 
                         default='~/Documents/design-space-toolbox/',
                         type=str,
@@ -36,16 +46,6 @@ def parse_arguments():
                         default='~/Documents/python-design-space-interface/', 
                         type=str,
                         help='directory for the c toolbox local git repository')
-    parser.add_argument('--toolbox_version', dest='toolbox_version',
-                        default=DEFAULT_TOOLBOX_VERSION, 
-                        type=str,
-                        help='update using indicated version of the c library')
-    parser.add_argument('--interface_version', dest='interface_version',
-                        default=DEFAULT_INTERFACE_VERSION, 
-                        type=str,
-                        help='update using indicated version of the python interface')
-    parser.add_argument('--linux', dest='is_linux', action='store_true',
-                        help='indicates if it should build using linux commands')
     args = parser.parse_args()
     return args
 
@@ -79,6 +79,8 @@ def update_c_toolbox(args):
     os.chdir(path.expanduser(args.toolbox_dict))
     call(['git', 'fetch', '--all'])
     version = args.toolbox_version
+    if version == '$RELEASE':
+        version=get_latest_release()
     if args.stable_or_release == 'release':
         version=get_latest_release()
     if args.stable_or_release == 'stable':
@@ -89,8 +91,10 @@ def update_c_toolbox(args):
         version = 'origin/'+version
         if version not in versions:
             raise ValueError, 'selected version of toolbox does not exist'
+            return 1
     else:
         version = 'tags/'+version
+    print 'Building ' + version
     call(['git', 'checkout', version])
     if args.is_linux is True:
         call(['sudo', 'make', 'install'])
@@ -99,12 +103,15 @@ def update_c_toolbox(args):
         call(['sudo', 'cp'] + os.listdir('build/Release/usr/local/include/') + ['/usr/local/include/designspace/'])
         call(['sudo', 'cp', 'build/Release/libdesignspace.dylib', '/usr/local/lib/'])
     os.chdir(pwd)
+    return 0
 
 def update_python_interface(args):
     pwd = os.getcwd()
     os.chdir(path.expanduser(args.interface_dict))
     call(['git', 'fetch', '--all'])
     version = args.interface_version
+    if version == '$RELEASE':
+        version=get_latest_release()
     if args.stable_or_release == 'release':
         version=get_latest_release()
     if args.stable_or_release == 'stable':
@@ -115,13 +122,17 @@ def update_python_interface(args):
         version = 'origin/'+version
         if version not in versions:
             raise ValueError, 'selected version of toolbox does not exist'
+            return 1
     else:
         version = 'tags/'+version
+    print 'Building ' + version
     call(['git', 'checkout', version])
     call(['sudo', 'python', 'setup.py', 'install'])
     os.chdir(pwd)
+    return 0
         
 if __name__ == '__main__':
     args=parse_arguments()
+    print args
     update_c_toolbox(args)
     update_python_interface(args)
