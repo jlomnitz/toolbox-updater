72e44d4efbf9b0a748cc38c733db76d5cbab6a6a
Jason Lomnitz <jlomn@ucdavis.edu>
2014-10-10 13:07:47 -0700

    Updated version information to first beta version.

--- toolbox_update.py
+++ toolbox_update.py
@@ -27,7 +27,7 @@ import argparse
 import os
 from os import path
 
-__version__ = '1.0.0a2'
+__version__ = '1.0.0b1'
 
 # Temporarly uses the develop-0.3.0 branch. In the future, this will be
 # changed back to develop and develop-0.3.0 will be deleted.
