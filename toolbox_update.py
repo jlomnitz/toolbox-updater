d6d28bbf91ef49ca5c6b60f96dc8daf412f66fa3
Jason Lomnitz <jlomn@ucdavis.edu>
2015-03-16 09:54:19 -0700

    Updated version of the toolbox updated.

--- toolbox_update.py
+++ toolbox_update.py
@@ -30,7 +30,7 @@ from os import path
 
 ## dependency_checks = {'
 
-__version__ = '1.4.0a0'
+__version__ = '1.4.0a1'
 
 # Temporarly uses the develop-0.3.0 branch. In the future, this will be
 # changed back to develop and develop-0.3.0 will be deleted.
