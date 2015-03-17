112932b739cfe2ef2ad5330fe131e7fce51b5db6
Jason Lomnitz <jlomn@ucdavis.edu>
2015-03-17 10:31:38 -0700

    Added license and added message indicating that the python repository was created.

--- toolbox_update.py
+++ toolbox_update.py
@@ -255,6 +255,7 @@ def update_repositories(args):
         os.chdir(path.expanduser(args.build_dir))
         dirs = os.listdir(path.expanduser(args.build_dir))
         if args.interface_dir not in dirs:
+            print 'Creating ' + args.interface_dir + ' at ' + os.chdir(path.expanduser(args.build_dir))
             call(['git',
                   'clone', 
                   'https://jglomnitz@bitbucket.org/jglomnitz/python-design-space-interface.git', 
